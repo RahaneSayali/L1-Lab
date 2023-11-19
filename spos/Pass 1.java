e747f2b1fed98157b705278fa2380ff91909ea6f
RahaneSayali <sayalipr15@gmail.com>
2023-11-19 16:09:42 +0530

    added hci

--- spos/Pass 1.java
+++ spos/Pass 1.java
@@ -1,223 +1,186 @@
 // Program to implement Pass-1 assembler.
-
-import java.io.*;
 import java.util.*;
 
-class Pass-1_Assembler
-{
-    public static void main(String args[])throws Exception
-{
-        int loc=0,loc1=0;
-        int j=0,k,sym=0,lit=0,v=0,w=0;
-
-        FileReader f1=new FileReader("in.txt");
-        BufferedReader b1=new BufferedReader(f1);
-
-        FileWriter f3=new FileWriter("symboltab.txt");
-        BufferedWriter b3=new BufferedWriter(f3);
-
-        FileWriter f4=new FileWriter("intermediate.txt");
-        BufferedWriter  b4=new BufferedWriter(f4);
-
-        FileWriter f5=new FileWriter("literaltab.txt");
-        BufferedWriter b5=new BufferedWriter(f5);
-
-    StringBuffer sb = new StringBuffer();
-    String s,s3,s4,tmp,sd,sl;
-    String s1[]=new String[4];
-    String s2[]=new String[4];
-    String s5[][]=new String[4][2];
- while((s=b1.readLine())!=null)
-        {
-            int flag=0;
-            int m=0;
-            StringTokenizer st=new StringTokenizer(s);
-            while(st.hasMoreTokens())
-            {
-            s1[m]=st.nextToken();
-            m++;
-            }
-            if(s1[1].equals("START"))
-            {
-                b4.write("AD 01");		
-                b4.write(" C "+s1[2]);		
-                loc=Integer.parseInt(s1[2]);
-                b4.newLine();
-            }
-            else if(s1[1].equals("END"))
-            {
-                for(int i=1;i<=lit;i++)
-                {
-                b4.write(loc+"  ");
-                b4.write("AD 02");
-                        
-                b4.write(" C "+i);		
-                loc=loc+1;
-                b4.newLine();
-            }
-            }
-            else
-            {
-                b4.write(loc+"  ");
-                if(s1[0].equals("-")!=true)
-                {
-                sym++;
-                b3.write(" "+s1[0]);
-                b3.write(" "+loc+" ");
-                }
-    FileReader f2=new FileReader("op.txt");
-    BufferedReader b2=new BufferedReader(f2);
-		while((sl=b2.readLine())!=null)
+class Assembler1{
+	
+	ArrayList<ArrayList<String>> ic = null;
+	ArrayList<ArrayList<String>> sym = null;
+	ArrayList<ArrayList<String>> lit = null;
+	ArrayList<ArrayList<String>> pol = null;
+	
+	public String[][] assignLC(String[][] asm)
+	{
+		int start_value = 0;
+		for(int i = 0;i<asm.length;i++)
 		{
-			j=0;
-			StringTokenizer se=new StringTokenizer(sl);
-			while(se.hasMoreTokens())
+			for(int j = 0;j<4;j++)
 			{
-				s2[j]=se.nextToken();
-				j++;
-			}
-			if(s1[1].equals(s2[0])==true)
-			{
-				for(int a=1;a<3;a++)
-				{
-					b4.write("  ");
-					b4.write("   "+s2[a]);
+				if(asm[i][j].equals("START")) {
+					start_value = Integer.parseInt(asm[i][j+1]);
 				}
 			}
-			if(s1[2].equals(s2[0])==true)
+			if(i==0)
 			{
-				for(int a=1;a<2;a++)
-				{
-					b4.write("  ");
-					b4.write("   "+s2[a]);
-				}
+				continue;
 			}
-					
+			start_value = start_value + 1;
+			asm[i][0] = String.valueOf(start_value);
 		}
-		if(s1[3].contains("="))
-			{
-
-                int i;flag=0;
-                for( i=0;i<lit;i++)
-                {
-                if(s5[i][1].equals(s1[3]))
-                {flag=1;
-                break;}
-                }if(flag!=1)
-{
-				lit++;
-s5[v][0]=Integer.toString(lit);
-s5[v][1]=s1[3];
-v++;
-				b5.newLine();
-				b5.write(" "+lit);
-				b5.write(" "+s1[3]);
-				//b6.write(" "+loc1+" ");
-				b4.write(" ");
-				b4.write(" "+"L  "+lit);
-}
-     else
-{
-b4.write(" ");
-b4.write(" "+"L  "+s5[i][0]);
-}
-				
-			}
-
-			else if((s1[3]).matches("[a-zA-Z]"))
-			{
-				//b4.write(" ");
-				b4.write(" "+"S  "+sym);	
-				
-			}
-		loc=loc+1;
-
-		b4.newLine();
-		
-		b4.newLine();
-		b3.newLine();
-
+		return asm;
 	}
-			
+	
+	public void printCode(String[][] asm)
+	{
+		for(int i = 0;i<asm.length;i++)
+		{
+			for(int j = 0;j<4;j++)
+			{
+				System.out.print("	" + asm[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public void generateOutput(String[][] asm,String[][] mot)
+	{
+		ArrayList<ArrayList<String>> ic = new ArrayList(asm.length);
+		ArrayList<ArrayList<String>> sym = new ArrayList(asm.length);
+		ArrayList<ArrayList<String>> lit = new ArrayList(asm.length);
+		ArrayList<ArrayList<String>> pol = new ArrayList(asm.length);
+		
+		for(int i = 0;i<asm.length;i++)
+		{
+			ic.add(new ArrayList());
+			sym.add(new ArrayList());
+			lit.add(new ArrayList());
+			pol.add(new ArrayList());
+		}
+		int sym_index = 0;
+		int lit_index = 0;
+		int pol_index = 0;
+		
+		for(int i = 0;i<asm.length;i++)
+		{
+			for(int j = 0;j<4;j++)
+			{
+				for(int m = 0;m<mot.length;m++)
+				{
+					if(asm[i][j].equals(mot[m][0]))
+					{
+						String icStr = "("+mot[m][1] + "," + mot[m][2]+")";
+						ic.get(i).add(icStr);
+					}
+				}
+				if(asm[i][j].equals(mot[15][0]) || asm[i][j].equals(mot[12][0]))
+				{
+					String polStr = "("+pol_index+","+asm[i][j] + "," + asm[i][0]+")";
+					pol.get(i).add(polStr);
+				}
+				if(asm[i][j].matches("[a-zA-Z]"))
+				{
+					String symStr = "("+ sym_index +","+ asm[i][j] + "," + asm[i][0]+")";
+					sym.get(i).add(symStr);
+					sym_index = sym_index + 1;
+					String icStr = "(S" +"," + asm[i][j]+")";
+					ic.get(i).add(icStr);
+				}
+				if(asm[i][j].matches("[0-9]+") && asm[i][j] != asm[i][0])
+				{
+					String litStr = "("+ lit_index +"," +asm[i][j] + "," + asm[i][0]+")";
+					lit.get(i).add(litStr);
+					lit_index = lit_index + 1;	
+				}
+				
+			}
+		}
+		this.ic = ic;
+		this.sym = sym;
+		this.lit = lit;
+		this.pol = pol;
+	}
 }
-        b4.close();
-        b3.close();
-        b5.close();
-        }
+
+public class PASS1 {
+
+	public static void main(String[] args) {
+		
+		String asm_code[][]= {
+				{"","","START","200",""},
+				{"","","MOVER","AREG","X"},
+				{"","","MOVEM","BREG","Y"},
+				{"","X","DS","1",""},
+				{"","","END","",""}
+		};
+		
+		String mot[][] = {
+				{"STOP","IS","00"},
+				{"ADD","IS","01"},
+				{"SUB","IS","02"},
+				{"MULT","IS","03"},
+				{"MOVER","IS","04"},
+				{"MOVEM","IS","05"},
+				{"COMP","IS","06"},
+				{"BC","IS","07"},
+				{"DIV","IS","08"},
+				{"READ","IS","09"},
+				{"PRINT","IS","10"},
+				
+				{"START","AD","01"},
+				{"END","AD","02"},
+				{"ORIGIN","AD","03"},
+				{"EQU","AD","04"},
+				{"LTORG","AD","05"},
+				
+				{"DS","DL","01"},
+				{"DC","DL","02"},
+				
+				{"AREG","RG","01"},
+				{"BREG","RG","02"},
+				{"CREG","RG","03"},
+		};
+		
+		Assembler1 assembler = new Assembler1();
+		String[][] asm_with_lc;
+		asm_with_lc = assembler.assignLC(asm_code);
+		assembler.printCode(asm_with_lc);
+		assembler.generateOutput(asm_with_lc, mot);
+		System.out.println("IC");
+		for(ArrayList<String> str:assembler.ic)
+		{
+			for(String st:str)
+			{
+				System.out.print(st);
+			}
+			System.out.println();
+		}
+		System.out.println("SYM");
+		for(ArrayList<String> str:assembler.sym)
+		{
+			for(String st:str)
+			{
+				System.out.print(st);
+			}
+			System.out.println();
+		}
+		System.out.println("LIT");
+		for(ArrayList<String> str:assembler.lit)
+		{
+			for(String st:str)
+			{
+				System.out.print(st);
+			}
+			System.out.println();
+		}
+		System.out.println("POOL");
+		for(ArrayList<String> str:assembler.pol)
+		{
+			for(String st:str)
+			{
+				System.out.print(st);
+			}
+			System.out.println();
+		}
+		
+	}
+
 }
-
-
-// Input files:
-
-// in.txt
-
-// - START 100
-// L1 MOVER AREG =3
-// - MOVEM BREG X
-// - SUB AREG =1
-// - LTORG
-// - MOVEM AREG Y
-// - BC any L1
-// - ADD CREG,4
-// X DC 6
-// Y DS 2
-// - END
-
-// op.txt
-
-// STOP IS 01
-// ADD IS 02
-// SUB IS 03
-// MUL IS 04
-// MOVER IS 05
-// MOVEM IS 06
-// START AD 01
-// END AD 02
-// ORIGIN AD 03
-// LTORG AD 04
-// AREG R1
-// BREG R2
-// CREG R3
-// DREG R4
-// DC DL 01
-// DS DL 02
-
-
-// *************************************OUTPUT********************************
-
-// symboltab.txt
-
-//  L1 100
-//  X 107
-//  Y 108
-
-
-// intermediate.txt
-
-// AD 01 C 100
-// 100       IS     05     R1  L  1
-
-// 101       IS     06     R2 S  1
-
-// 102       IS     03     R1  L  2
-
-// 103       AD     04     R1  L  2
-
-// 104       IS     06     R1 S  1
-
-// 105  
-
-// 106       IS     02
-
-// 107       DL     01
-
-// 108       DL     02
-
-// 109  AD 02 C 1
-
-// 110  AD 02 C 2
-
-// literaltab.txt
-
-//  1 =3
-//  2 =1
